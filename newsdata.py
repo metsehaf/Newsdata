cec166f46333731184759f2218b48e6c3cb32185
Girum Hagos <hagosgirum@gmail.com>
2017-07-22 02:53:52 -0400

    update newsdata.py

--- newsdata.py
+++ newsdata.py
@@ -1,72 +1,84 @@
+#! /usr/bin/env python3
 """Database code to connect to newsdata.sql."""
 
 import psycopg2
 
 DBNAME = "news"
 
+# create a function that establishes the database connection
+
+
+def connect(database_name="news"):
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print("<error message>")
+
+
 def Question_1():
-    #connect to database news
-    db = psycopg2.connect("dbname=news")
+    # connect to database news
+    db, cursor = connect()
 
-    c = db.cursor()
-
-#get the top 3 articles based on the amount of views
+# get the top 3 articles based on the amount of views
 
     query = "select * from Q1;"
-    c.execute(query)
+    cursor.execute(query)
 
-#fetch the results
-    rows = c.fetchall()
+# fetch the results
+    rows = cursor.fetchall()
 
-#Loop through each title, count pairing and print out data for top 3. Reference site https://pyformat.info
-    for row in rows:#[:3]:
+# Loop through each title, count pairing and print out data for top 3.
+    for row in rows:
         print("\nArticle: {:^10} \nViews: {:^10}" .format(row[0], row[1]))
-    #print("The Top Three Articles are: ")
-        #print("%s " % (row[0]))
+# print("The Top Three Articles are: ")
+# print("%s " % (row[0]))
 
-#close database
+# close database
     db.close()
+
 
 def Question_2():
-    #connect to database news
-    db = psycopg2.connect("dbname=news")
+    # connect to database news
+    db, cursor = connect()
 
-    c = db.cursor()
-
-#get the top 3 articles based on the amount of views
+# get the top 3 articles based on the amount of views
 
     query = "select * from Q2;"
-    c.execute(query)
+    cursor.execute(query)
 
-#fetch the results
-    rows = c.fetchall()
+# fetch the results
+    rows = cursor.fetchall()
 
-#Loop through each title, count pairing and print out data for top 3. Reference site https://pyformat.info
+# Loop through each title, count pairing and print out data for top 3
     for row in rows:
-        #print(row[0])
-    #print("The Top Three Articles are: ")
+        # print("The Top Three Articles are: ")
         print("\nAuthor: {:^10} \nViews: {:^10}" .format(row[0], row[1]))
-#close database
+# close database
     db.close()
 
+
 def get_error_percentage():
-    #connect to database news
-    db = psycopg2.connect("dbname=news")
 
-    c = db.cursor()
+    # connect to database news
+    db, cursor = connect()
 
-#get the top 3 articles based on the amount of views
+# get the top 3 articles based on the amount of views
 
-    query = " select date(time) AS date from log group by date;"
-    c.execute(query)
+    query = '''SELECT time::date AS date, (SELECT count(status)::float FROM log)
+     AS total , (Select count(status)::float FROM log WHERE status LIKE
+     '%404 %') AS error FROM log GROUP BY date ORDER BY date, total, error'''
+    cursor.execute(query)
 
-#fetch the results
-    rows = c.fetchall()
+# fetch the results
+    rows = cursor.fetchall()
 
-#Loop through each title, count pairing and print out data for top 3. Reference site https://pyformat.info
+# Loop through each title, count pairing and print out data for top 3
     for row in rows:
-        print("%s " % (row[0]))
-#runs the function to start the queries
+        total = (row[2]/row[1]) * 100
+        print(row[0], total)
+# Runs the function to start the queries
 if __name__ == '__main__':
     Question_1()
     Question_2()
